905624453f0fb20c2a66f7c24ad481b989eb54f3
ryszardcopa <ryszard.copa@hotmail.com>
2020-05-26 15:20:23 +0200

    added place for calculation & prices

--- scripts/NewPassengerScript.js
+++ scripts/NewPassengerScript.js
@@ -5,3 +5,11 @@ function myFunction() {
     window.open('./index.html', '_self');
   }, 10800000);
 }
+
+// --- FLIGHT PRICES AND PLANES --- //
+
+// --- EUROPEAN ---//
+$Kat_Fran = 300;
+$Fran_LA = 1500;
+$Kat_LA = 1200;
+// --- TRANSATLANTIC --- //
